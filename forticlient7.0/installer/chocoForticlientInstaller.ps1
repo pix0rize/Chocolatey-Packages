aacb112a0e51f785dc15b39a14d4ae5750472c11
pix0rize <106530334+pix0rize@users.noreply.github.com>
2022-05-30 14:50:54 +0200

    Update chocoForticlientInstaller.ps1

--- forticlient7.0/installer/chocoForticlientInstaller.ps1
+++ forticlient7.0/installer/chocoForticlientInstaller.ps1
@@ -1,1 +1,22 @@
-##
+$ErrorActionPreference = 'Stop'
+$packageName   = 'forticlient70'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url           = 'http://www.romeropolo.com/vpn/FortiClientVPNOnlineInstaller.exe'
+$checksum      = '51c5bd277d73ff062ad4a083fa6cd9a718748caa18659900b17f1255cbff508e'
+$url64         = 'http://www.romeropolo.com/vpn/FortiClientVPNOnlineInstaller.exe'
+$checksum64    = '51c5bd277d73ff062ad4a083fa6cd9a718748caa18659900b17f1255cbff508e'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64         = $url64
+  silentArgs    = '/QUIET /NORESTART'
+  softwareName  = 'VPN FortiClient' 
+  checksum      = $checksum
+  checksum64    = $checksum64
+  checksumType  = 'sha256'
+  }
+
+Install-ChocolateyPackage @packageArgs
